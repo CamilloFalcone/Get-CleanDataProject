eb9f13e8170e1723741c84005e589195247df11a
Camillo Falcone <camillo.falcone@gmail>
2016-06-10 07:12:00 +0200

    final

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # Get-CleanDataProject
-This repo provides a solution for the final assignement in the 
-getting and cleaning data course in the Data Science Johns Hopkins Specialiation
+This repo includes a script to create two tidy date sets extracted from the raw data set resulted from a wearable computing experiment.
+During such experiment, as described in activityrecognition@smartlab.ws, a set of measurement has been taken from 30 users using a wearable device.
 
+The script is organized around a Main function which performs the following steps:
+	asking the user for the path name where the original raw data files are available
+	checking for the existence of the path provided
+	set the working directory to the provided path
+	merging the test and train files into a unique set of files of features, activities and subjects
+	process the the data in order to produce the first data frame 
+	save the data frame into a file located in the folder initially provided by the user (tidyset1.txt) 
+	use the first data set to produce a second one, which is then saved into a file (tidyset2.txt), located in the same folder as the previous one
+
+More details on the programming solutions are provided as comments in the script code.
+Details on the data set provided are described in the CodeBook.
+	
+	
--- run_analysis.R
+++ run_analysis.R
@@ -1,15 +1,12 @@
-InitFileName<-function(root){
+CreateTmpFiles<-function(root){
+	#merge the training and test data set files into unique files of features, activities identifiers and subject identifiers
 TestFile<-paste(root, "/test/X_test.txt", sep="")
 TrainFile <-paste(root, "/train/X_train.txt", sep="")
 TestActivityFile<-paste(root, "/test/y_test.txt", sep ="")
 TrainActivityFile<-paste(root, "/train/y_train.txt", sep ="")
 TestSubjectFile<-paste(root, "/test/subject_test.txt", sep="")
 TrainSubjectFile<- paste(root, "/train/subject_train.txt", sep="")
-	
-}
 
-CreateTmpFiles<-function(){
-	#merge training and test data set
 file.create("Merge.txt")
 file.append("Merge.txt", TestFile)
 file.append("Merge.txt", TrainFile)
@@ -19,81 +16,98 @@ file.append("Activity.txt", TrainActivityFile)
 file.create("Subject.txt")
 file.append("Subject.txt", TestSubjectFile)
 file.append("Subject.txt", TrainSubjectFile)
-	
+
 }
 
+
 RemoveTmpObjects<-function(){
-	#remove all temporary files and objects
+	#remove all temporary files 
 	file.remove("Merge.txt", "Activity.txt", "Subject.txt")
-	rm(All, k, mydata, mydata1)
 }
 
 Read_FormatDataSet1<-function(){
+	#returns the data frame containing the first tidy set
+	#
 	features<-read.table("features.txt", sep=" ") #the list of features
-	index<-grepl("mean|std|Mean", features[,2]) #the index of the names of the features which are mean or standard dev
+	index<-grepl("mean|std|Mean", features[,2]) #the index of the names of the features which contains mean or standard dev values
 	labels<-read.table("activity_labels.txt", sep = " ") #the list of the activity names
-	activities<-read.table("Activity.txt") # the 
-	outtest<-merge(activities, labels, by.x="V1", by.y="V1")
-	dimnames(outtest)[[2]]<-c("V1", "Activity") # fix the name of the activity column
-	All<-scan("Merge.txt") #the entire set of measurements as a numeric vector
-	k<-matrix(All, ncol=561) #the 561 columns
+	RawActivities<-read.table("Activity.txt") # the entire set of activities performed by subjects in the experiment
+	Activities <-merge(RawActivities, labels, by.x="V1", by.y="V1") # the set of activities is labelles with the proper name
+	dimnames(Activities)[[2]]<-c("V1", "Activity") # fix the name of the activity column to a understandable label
+	Measures <-scan("Merge.txt") #the entire set of measurements is read as a numeric vector
+	k<-matrix(Measures, ncol=561) #the flat list of measure is transformed in a matrix of observation ( 561 columns)
 	mydata<-data.frame(k) # the complete measurement data frame
-	mydata1<-mydata[,index] #only 86 variables
-	features1<-features[,2][index] #only 86 names for columns
-	rowname<-c(1:10299)
+	mydata1<-mydata[,index] #from the complete data set only 86 variables are extracted, the features which contain mean or sd values
+	features1<-features[,2][index] #the name of the 86 variables is extracted from the entire set of feature names
+	rowname<-c(1:dim(mydata[1])) # the array to use for fix the name of the rows
 #set the name for the variable...
-dimnames(mydata1)<-list(rowname, features1)
-mydata1<- cbind(mydata1, outtest[2]) #THIS IS THE FIRST TIDY SET...
+	dimnames(mydata1)<-list(rowname, features1)
+	mydata1<- cbind(Activities[2], mydata1) #THIS IS THE FIRST TIDY SET: it contains the subset of the 86 variables in the original complete data set and a new column with the variable Activity with a proper name
 return(mydata1)
+}
 
-
-
+RenameVar<-function(x){
+	# to rename the variable for the second data set adding an "avg" prefix
+		#if x == Subject or x == Activity skip the renaming..
+		if(x =="Subject" | x == "Activity") {
+			return(x)
+		}
+		else {
+	return (paste("avg[", x, "]", sep = ""))
+	}
 }
 
 Read_FormatDataSet2<-function(ds){
 	#create a 2nd indipendent data set with the average for each activity and each subject
 mysubjects<-read.table("Subject.txt")
 dimnames(mysubjects)[[2]]<-c("Subject")
-mydata2<-cbind(ds, mysubjects) # this is the 2nd data set to summarize
-
+mydata2<-cbind(mysubjects, ds) # this is the 2nd data set to summarize
+n1<-names(mydata2) # the array of original variable names to convert into avg[<varname>]
+n2<-sapply(n1, RenameVar) #n2 is the vector of new names used to rename the data set
+dimnames(mydata2)[[2]]<-n2
 library(dplyr)
 
 #group_by and then summarize...
 grouped <- group_by(mydata2, Subject, Activity)
-k<-summarize(grouped, mean(`tBodyAcc-mean()-X`))
-# problema: come nominare tutte le variabili...
+k<-summarize_each (grouped, funs(mean)) # the summarize_each function allows to summarize the entire set of variabile
 return(k)
-
-
 	
 }
 
-Main<-function() {
-wd<-getwd() #to reset at the end of the script
-#RootFolder<-"/DataScienceFolder/UCI HAR Dataset"
-RootFolder<-readline("Provide the path to the project data folder: ")
-# read the folder root as an input and check if it exists..
-if (!file.exists(RootFolder)) {
-	cat ("Root folder not found")
+AskRawDataReference<-function(){
+#ask the user for the path of the root folder where raw data are available
+# check for the existance of the provided path
+# if exists returns the Root Folder otherwise returns the "0" char
+	RootFolder <-readline("Provide the path to the root of the raw data folders: ")
+	if (!file.exists(RootFolder)) {
+	
+	return("0")
 	
 } else
 {
+return(RootFolder)
+}
+}
+
+
+
+
+Main<-function() {
+	# this is the core of the script, see the ReadMe for a description of the steps
+wd<-getwd() #the initial working directory is stored in order to reset it at the end of the script
+RootFolder<- AskRawDataReference()
+if( RootFolder == "0") {
+	cat ("Root folder not found")
+}
+else {	
+#RootFolder<-"/DataScienceFolder/UCI HAR Dataset" <--this is the value of my root folder (Mac syntax)
 setwd(RootFolder)
-
-InitFileName(RootFolder)
-CreateTmpFiles()
-
+CreateTmpFiles(RootFolder)
 ds1<-Read_FormatDataSet1()
-write.table(ds1, "tidyset1.txt")
-
+write.table(ds1, "tidyset1.txt", row.names=FALSE)
 ds2<- Read_FormatDataSet2(ds1)
-write.table(ds2, "tidyset2.txt")
-
-#remove all environment variables and temporary files used 
-
+write.table(ds2, "tidyset2.txt", row.names=FALSE)
 RemoveTmpObjects()
-
-
 setwd(wd)
 }
 }
